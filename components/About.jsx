b39ad61da1eba21cded65eb52b2c0bd4f94fbffe
dalonso <masdavidalonso@gmail.com>
2025-08-26 18:08:32 +0200

    Initial commit: My first React project using Vite

---
+++ components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+   return (
+      <section>
+         <h2>About</h2>
+         <p>I'm a frontend developer dedicated to building sleek, intuitive interfaces and automating the mundane. I'm obsessed with best practices and driven by a constant desire to learn and master new technologies.</p>
+      </section>
+   )
+}
---
+++ components/Interests.jsx
@@ -0,0 +1,8 @@
+export default function Interests() {
+   return (
+      <section className="interests">
+         <h2>Interests</h2>
+         <p>Optimist (Deutsch-style). Rapid Learner. Systems Thinker. Ex-Bar Manager. Cocktail Crafter. Design Enthusiast. Tech Aficionado. Ocassional Speedcuber (&lt;60s)</p>
+      </section>
+   )
+}
